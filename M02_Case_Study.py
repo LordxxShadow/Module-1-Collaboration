1d82267c0240747e59257520fdde2d4897e59ac9
Shadow <ojeyemimarvelous@gmail.com>
2026-09-04 09:05:46 -0400

    Add files via upload

---
+++ M02_Case_Study.py
@@ -0,0 +1,18 @@
+# Marvelours Ojeyemi
+# M02_Case_Study.py
+# This program accepts student names and GPAs and determines whether
+# each student qualifies for the Dean's List or Honor Roll.
+while True:
+    last_name = input("Enter the student's last name (ZZZ to quit): ")
+
+    if last_name == "ZZZ":
+        break
+
+    first_name = input("Enter the student's first name: ")
+    gpa = float(input("Enter the student's GPA: "))
+
+    if gpa >= 3.5:
+        print(first_name, last_name, "has made the Dean's List.")
+
+    if gpa >= 3.25:
+        print(first_name, last_name, "has made the Honor Roll.")
